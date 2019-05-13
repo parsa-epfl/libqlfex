9c84730ff046624adced19b8e108de1971cb0cc3
Rafael Ulises Luzius Pizarro Solar <rafael.pizarrosolar@epfl.ch>
2019-05-13 14:01:00 +0200

    ARMFLX: API style improvement, and cleaner interface

--- armflex-helper.c
+++ armflex-helper.c
@@ -10,15 +10,16 @@
 #define ARCH_NUM_REGS           (32)
 #define ARCH_XREGS_SIZE         (ARCH_NUM_REGS * sizeof(uint64_t))
 #define ARCH_XREGS_OFFST        (0)
-#define ARCH_PC_OFFST           (32 * 2)
-#define ARCH_PSTATE_FLAG_OFFST  (ARCH_PC_OFFST + 2)
-#define ARCH_PSTATE_CF_OFFST    (ARCH_PSTATE_FLAG_OFFST + 0)
-#define ARCH_PSTATE_VF_OFFST    (ARCH_PSTATE_FLAG_OFFST + 1)
-#define ARCH_PSTATE_NF_OFFST    (ARCH_PSTATE_FLAG_OFFST + 2)
-#define ARCH_PSTATE_ZF_OFFST    (ARCH_PSTATE_FLAG_OFFST + 3)
+#define ARCH_PC_OFFST           (32 * 2)                        // 64
+#define ARCH_PSTATE_FLAG_OFFST  (ARCH_PC_OFFST + 2)             // 66
+#define ARCH_PSTATE_CF_OFFST    (ARCH_PSTATE_FLAG_OFFST + 0)    // 66
+#define ARCH_PSTATE_VF_OFFST    (ARCH_PSTATE_FLAG_OFFST + 1)    // 67
+#define ARCH_PSTATE_NF_OFFST    (ARCH_PSTATE_FLAG_OFFST + 2)    // 68
+#define ARCH_PSTATE_ZF_OFFST    (ARCH_PSTATE_FLAG_OFFST + 3)    // 69
+
 
 void* armflex_pack_archstate(CPUARMState *cpu) {
-    uint32_t *buffer = malloc((ARCH_PSTATE_ZF_OFFST + 1) * sizeof(uint32_t));
+    uint32_t *buffer = malloc(ARCH_STATE_SIZE);
 
     memcpy(&buffer[ARCH_XREGS_OFFST],     &cpu->xregs, ARCH_XREGS_SIZE);
     memcpy(&buffer[ARCH_PC_OFFST],        &cpu->pc, sizeof(uint64_t));
@@ -39,52 +40,62 @@ void armflex_unpack_archstate(uint32_t *buffer, CPUARMState *cpu) {
     cpu->ZF = buffer[ARCH_PSTATE_ZF_OFFST];
 }
 
-int armflex_write_page(void* src, target_ulong size) {
+int armflex_write_file(const char *filename, void* buffer, target_ulong size) {
 
+    char filepath[PATH_MAX];
     int fd = -1;
     void *region;
-    if (mkdir("/tmp/armflex", 0777) && errno != EEXIST) {
-        printf("mkdir /tmp/armflex");
+
+    if (mkdir(ARMFLEX_ROOT_DIR, 0777) && errno != EEXIST) {
+        printf("'mkdir "ARMFLEX_ROOT_DIR"' failed\n");
         return 1;
     }
-    if((fd = open("/tmp/armflex/program_page", O_RDWR | O_CREAT, 0666)) == -1) {
-        printf( "Program Page dest file: open failed");
+    sprintf(filepath, ARMFLEX_ROOT_DIR"/%s", filename);
+    if((fd = open(filepath, O_RDWR | O_CREAT, 0666)) == -1) {
+        printf("Program Page dest file: open failed\nfilepath:%s\n", filepath);
         return 1;
     }
     if (lseek(fd, size-1, SEEK_SET) == -1) {
         close(fd);
-        perror("Error calling lseek() to 'stretch' the file");
+        printf("Error calling lseek() to 'stretch' the file\n");
         return 1;
     }
     if (write(fd, "", 1) != 1) {
         close(fd);
-        perror("Error writing last byte of the file");
-        return 1;
-    }
-    region = mmap(NULL, size, PROT_READ|PROT_WRITE, MAP_SHARED, fd, 0);
-    if(region == MAP_FAILED) {
-        printf("Program Page dest file: mmap failed");
+        printf("Error writing last byte of the file\n");
         return 1;
     }
 
-    memcpy(region, src, size);
+    region = mmap(NULL, size, PROT_READ|PROT_WRITE, MAP_SHARED, fd, 0);
+    if(region == MAP_FAILED) {
+        close(fd);
+        printf("Error dest file: mmap failed");
+        return 1;
+    }
+
+    memcpy(region, buffer, size);
+    msync(region, size, MS_SYNC);
     munmap(region, size);
+
     close(fd);
     return 0;
 }
 
-void armflex_page_sanity_check(size_t offst) {
+void armflex_page_sanity_check(const char* filename, size_t offst,  uint32_t expected) {
+    char filepath[PATH_MAX];
     int fd = -1;
-    if((fd = open("/tmp/armflex/program_page", O_RDWR, 0666)) == -1) {
-        printf( "Program Page dest file: open failed");
+    uint32_t res;
+    int bytes;
+
+    sprintf(filepath, ARMFLEX_ROOT_DIR"/%s", filename);
+    if((fd = open(filepath, O_RDWR, 0666)) == -1) {
+        printf("Program Page dest file: open failed.\nfilepath:%s\n", filepath);
     }
     if (lseek(fd, offst, SEEK_SET) == -1) {
         close(fd);
-        perror("Error calling lseek() to 'stretch' the file");
+        printf("lseek failed on: %s", filepath);
     }
-    uint32_t insns;
-    int bytes = read(fd, &insns, 4);
-    qemu_log_mask(ARMFLEX_LOG,
-                  "BYTES:%d|PHYS_INS_FROM_PAGE:0x%x\n\n", bytes, insns);
+    bytes = read(fd, &res, 4);
+    assert(res == expected);
     close(fd);
 }
--- armflex-helper.h
+++ armflex-helper.h
@@ -1,6 +1,10 @@
 #include "../qemu/include/exec/cpu-defs.h"
 
+#define ARCH_STATE_SIZE         ((70) * sizeof(uint32_t))
+
+#define ARMFLEX_ROOT_DIR        "/tmp/armflex"
+
 void* armflex_pack_archstate(CPUARMState *cpu);
 void armflex_unpack_archstate(uint32_t *buffer, CPUARMState *cpu);
-int armflex_write_page(void* src, target_ulong size);
-void armflex_page_sanity_check(size_t offst);
+int armflex_write_file(const char *filename, void* buffer, target_ulong size);
+void armflex_page_sanity_check(const char* filename, size_t offst,  uint32_t expected);
